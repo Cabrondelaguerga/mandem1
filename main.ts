57e318f8d896bc78d2b9f71db66bd2c6ced317fc
Cabrondelaguerga <113055621+Cabrondelaguerga@users.noreply.github.com>
2022-09-07 14:11:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Cabron")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("mandem")
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Skull)
+})
